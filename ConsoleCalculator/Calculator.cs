03d8c28b97760584ea5f128e77f67d7878c6f9a8
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-25 15:14:28 +0000

    Ajoutez des fichiers projet.

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int val1, int val2, string operation)
+        {
+            //if(operation == null)
+            //{
+            //    throw new NullReferenceException("operation");
+            //}
+
+            string nonNullOperation = operation ?? throw new NullReferenceException(nameof(operation));
+
+
+            if (nonNullOperation == "/")
+            {
+                return Divide(val1, val2);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("operation","Non recognized operation");
+            }
+        }
+
+        private int Divide(int val1, int val2)
+        {
+            return val1/val2 ;
+        }
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var divide = new Calculator();
+
+            var input1 = int.Parse(Console.ReadLine());
+            
+            var input2 = int.Parse(Console.ReadLine());
+            var operation = Console.ReadLine();
+
+            try
+            {
+                var result = divide.Calculate(input1, input2, null);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"This is {ex}");
+            }
+            
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
